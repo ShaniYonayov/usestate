310b64b11408280e04b5f0242fca35694a67942b
Shani Yonayov <shanipninayonayov1@gmail.com>
2022-11-16 19:43:05 +0200

    Added useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Counter from './Counter';
 
 function App() {
   return (
     <div className="App">
       Usestate
       <p>Another line</p>
+      <Counter />
     </div>
   );
 }

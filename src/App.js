238ceacb407eadbfc63a8f88f4d1f13c14f29f6a
Shani Yonayov <shanipninayonayov1@gmail.com>
2022-11-27 17:00:22 +0200

    onClass4-27.11-2Counters1Delta

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
 import Counter from './Counter';
+import { useState } from 'react';
+// הוא גם משתמש ביוזסטייט אז צריך להביא אותו
+
 
 function App() {
+  const [delta, setDelta] = useState(1)
+
+function handelDelta(e){
+  console.log(e);
+  setDelta(Number(e.target.value))
+}
+// כל זה צריך להיות בתוך האפ
   return (
     <div className="App">
       Usestate
-      <Counter />
+      <input type="number" value = {delta} onChange = {handelDelta} />
+      <Counter delta = {delta} />
+      <Counter delta = {delta} />
+      {/* הוספנו עוד שעון */}
+      {/* אם נרצה דלתא משותף ל2 השעונים נצטרך להוציא את כל מה שקשור לדלתא מהקאונטר */}
+    {/* צריך להעביר את הפרופס
+    פה אלמנט האב מעביר לאלמנט הבן את הסטייט */}
     </div>
   );
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
-function Counter() {
+function Counter(props) {
+    // פרופס זה אובייקט שמחיל את כל המשתנים שאלמנט האב העביר לאלמנט הבן
+    const {delta} = props
+    // נמשוך את הדלתה מהפרופס
     const [count, setCount] = useState(1) 
-    const [delta, setDelta] = useState(1)
-    // הגדרנו את המשתנה עם ערך התחלתי של 1
+
     function incr(){
         setCount(      
             function(oldCount){
@@ -13,27 +15,12 @@ function Counter() {
     function reset(){
       setCount(0)     
     }
-    function handelDelta(e){
-    //   זאת פונק שתלויה בשינוי ובניגוד לקליק פה זה תלוי בשינוי אז נכניס פרמטר אי
-       console.log(e);
-       setDelta(Number(e.target.value))
-    //    ככה תופסים את הערך של  שדה אינפוט, מה שיש בתוך האי
-    // אם לא היינו מוסיפים תנמבר, היינו מקבלים מחרוזת, כלומר להוסיף 6 ל1 נקבל 61
-    // לא תתבצע סכימה של המספרים ולכן יש להמיר את הסוג מסטרינג לנאמבר
-    // אי לא מילה שמורה
-    }
+
      return (
       <div>
         <h1>Counter</h1>
-        <input type="number" value = {delta} onChange = {handelDelta} />
-        {/*  נכניס משתנה מסוג אינפוט שהברירת מחדל שלו "1" בוליו, ה1 לא משתנה, 
-        צריך להגדיר סטייט, אני לא יכולה לשנות שום דבר בלי סטייט ויש להגדיר פונק */}
-        {/* לכן נשנה תוליו למשתנה {דלתא} שיצרנו */}
-        {/* און ציינג זה מה השינוי בעצם שקורה כשמכניסים אינפוט, 
-        ולתוכו נכניס את הפונק שמנהלת את סאט דלתא - הנדל דאלטה */}
         <p>Counter is at {count} </p>
         <button onClick={incr}>Click to add {delta} to Counter</button>
-        {/* נרצה שבכפתור יהיה כתוב שנוסיף את ערך הדלתא לקאונטר */}
         <button onClick={reset}>Reset Counter</button>
       </div>
     );

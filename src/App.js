2de64e7febcdea7c85479cc86eecaceeeb827677
Shani Yonayov <shanipninayonayov1@gmail.com>
2022-11-28 13:55:40 +0200

    homework3-AddMaxToCounter

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import './App.css';
 import Counter from './Counter';
 import { useState } from 'react';
-// הוא גם משתמש ביוזסטייט אז צריך להביא אותו
+
 
 
 function App() {
   const [delta, setDelta] = useState(1)
+  const [max, setMax] = useState(1)
 
 function handelDelta(e){
   console.log(e);
   setDelta(Number(e.target.value))
 }
-// כל זה צריך להיות בתוך האפ
+function handelMax(m){
+  setMax(Number(m.target.value))
+  }  
+
   return (
     <div className="App">
-      Usestate
+     <h1>Usestate</h1>
+     <label> Enter Counter jumping number
       <input type="number" value = {delta} onChange = {handelDelta} />
-      <Counter delta = {delta} />
-      <Counter delta = {delta} />
-      {/* הוספנו עוד שעון */}
-      {/* אם נרצה דלתא משותף ל2 השעונים נצטרך להוציא את כל מה שקשור לדלתא מהקאונטר */}
-    {/* צריך להעביר את הפרופס
-    פה אלמנט האב מעביר לאלמנט הבן את הסטייט */}
+      </label>
+      <label>Enter Counter MAX number
+        <input type="number" value = {max} onChange = {handelMax}/ >
+      </label>
+      <Counter delta = {delta} max = {max} />
+      <Counter delta = {delta} max = {max} />
     </div>
   );
 }
